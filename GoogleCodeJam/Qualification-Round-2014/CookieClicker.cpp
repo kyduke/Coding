f7e59cd8833978f43ae838505d921a39d8aaebd6
kyduke <kyduke@gmail.com>
2015-03-19 22:50:14 +0900

    Update CookieClicker.cpp

--- GoogleCodeJam/Qualification-Round-2014/CookieClicker.cpp
+++ GoogleCodeJam/Qualification-Round-2014/CookieClicker.cpp
@@ -1,65 +1,78 @@
+//
+//  main.cpp
+//  GoogleCodeJam_QualificationRound2014_CookieClicker
+//
+//  Created by Youngduke Koh on 3/19/15.
+//  Copyright (c) 2015 Youngduke Koh. All rights reserved.
+//
+
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <stdio.h>
 
 using namespace std;
 
 double solve(double C, double F, double X)
 {
-	double rate, next, min, temp;
-
-	rate = 2.0;
-	min = X / rate;
-	next = 0;
-
-	while (1)
-	{
-		next += C / rate;
-		rate += F;
-		temp = next + X / rate;
-		if (temp < min)
-		{
-			min = temp;
-		}
-		else
-		{
-			break;
-		}
-	}	
-
-	return min;
+    double rate, next, min, temp;
+    
+    rate = 2.0;
+    min = X / rate;
+    next = 0;
+    
+    while (1)
+    {
+        next += C / rate;
+        rate += F;
+        temp = next + X / rate;
+        if (temp < min)
+        {
+            min = temp;
+        }
+        else
+        {
+            break;
+        }
+    }
+    
+    return min;
 }
 
 int main(int argc, char* argv[])
 {
-	int T, i;
-	double C, F, X;
-
-	cin >> T;
-	i = 0;
-	while (i < T)
-	{
-		i++;
-		cin >> C >> F >> X;
-
-		cout << "Case #" << i << ": ";
-
-		printf("%.7f", solve(C, F, X));
-
-		cout << "\n";
-	}
-
-	return 0;
+    int T, i;
+    double C, F, X;
+    
+    ifstream in("in.txt");
+    cin.rdbuf(in.rdbuf());
+    
+    cin >> T;
+    i = 0;
+    while (i < T)
+    {
+        i++;
+        cin >> C >> F >> X;
+        
+        cout << "Case #" << i << ": ";
+        
+        printf("%.7f", solve(C, F, X));
+        
+        cout << "\n";
+    }
+    
+    return 0;
 }
 
 /*
-4
-30.0 1.0 2.0
-30.0 2.0 100.0
-30.50000 3.14159 1999.19990
-500.0 4.0 2000.0
-=====
-Case #1: 1.0000000
-Case #2: 39.1666667
-Case #3: 63.9680013
-Case #4: 526.1904762
-*/
+ 4
+ 30.0 1.0 2.0
+ 30.0 2.0 100.0
+ 30.50000 3.14159 1999.19990
+ 500.0 4.0 2000.0
+ =====
+ Case #1: 1.0000000
+ Case #2: 39.1666667
+ Case #3: 63.9680013
+ Case #4: 526.1904762
+ */
