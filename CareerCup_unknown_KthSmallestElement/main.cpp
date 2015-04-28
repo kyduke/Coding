a4c5d60aa910e4ca178baf3d57678215dadc63db
kyduke <kyduke@gmail.com>
2015-04-28 11:46:50 +0900

    Create main.cpp

---
+++ CareerCup_unknown_KthSmallestElement/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int getKthSmallestElement(vector<int> num, int k) {
+	set<int> s;
+	set<int>::iterator it;
+	int i, l;
+
+	l = num.size();
+	for (i = 0; i < l; i++) {
+		s.insert(num[i]);
+	}
+
+	i = 0;
+	for (it = s.begin(); it != s.end(); it++) {
+		i++;
+		if (i == k) return *it;
+	}
+
+	return -1;
+}
+
+int main(int argc, char* argv[]) {
+	int arr[] = {12, 3, 17, 0, 9, 6, 100};
+	vector<int> num(arr, arr + sizeof(arr) / sizeof(arr[0]));
+
+	cout << getKthSmallestElement(num, 3) << "\n";
+
+	return 0;
+}
