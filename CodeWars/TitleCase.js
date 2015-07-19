55e1bb65bc1fd8da3d353c536c6db067209ba2d2
kyduke <kyduke@gmail.com>
2015-07-20 00:47:52 +0900

    Create TitleCase.js

---
+++ CodeWars/TitleCase.js
@@ -0,0 +1,14 @@
+function titleCase(title, minorWords) {
+    var titleArray, minorArray, i;
+    
+    titleArray = title.toLowerCase().split(' ');
+    minorArray = (minorWords) ? minorWords.toLowerCase().split(' ') : [];
+    
+    for (i in titleArray) {
+        if (i != 0 && minorArray.indexOf(titleArray[i]) >= 0) continue;
+        if (titleArray[i].length == 0) continue;
+        titleArray[i] = titleArray[i][0].toUpperCase() + titleArray[i].substring(1);
+    }
+    
+    return titleArray.join(' ');
+}
