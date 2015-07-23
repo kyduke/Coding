57502e5ce5767f42998f2dc7f94d509cbe13ce52
kyduke <kyduke@gmail.com>
2015-07-23 12:12:03 +0900

    Create GeraldsHexagon.cpp

---
+++ CodeForces/CodeForces_559/GeraldsHexagon.cpp
@@ -0,0 +1,36 @@
+// http://codeforces.com/problemset/problem/559/A
+
+#include <iostream>
+
+using namespace std;
+
+int a, b, c, d, e, f;
+
+int solve() {
+	int count;
+
+	count = (a + f + b) * (a + f + b);
+	count -= f * f;
+	count -= b * b;
+	count -= d * d;
+
+	return count;
+}
+
+int main(int argc, char* argv[]) {
+	cin >> a >> b >> c >> d >> e >> f;
+
+	cout << solve() << "\n";
+
+	return 0;
+
+
+/*
+1 1 1 1 1 1
+=====
+6
+
+1 2 1 2 1 2
+=====
+13
+*/
