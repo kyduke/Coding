067d85eee37d89cd25e7ab8de5b988fb970f8472
kyduke <kyduke@gmail.com>
2015-09-25 11:45:59 +0900

    Update 231_PowerOfTwo.cpp

--- LeetCode/231_PowerOfTwo.cpp
+++ LeetCode/231_PowerOfTwo.cpp
@@ -7,7 +7,7 @@ using namespace std;
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-		if (n == 0) return false;
+		if (n <= 0) return false;
 		while (n > 1) {
 			if (n % 2 == 1) {
 				return false;
