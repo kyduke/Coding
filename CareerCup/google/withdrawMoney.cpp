61c875b18e999f7b98682ddedc84f25a393fd11b
kyduke <kyduke@gmail.com>
2015-10-14 16:10:49 +0900

    Update withdrawMoney.cpp

--- CareerCup/google/withdrawMoney.cpp
+++ CareerCup/google/withdrawMoney.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 const UINT16 MAX_UINT16 = 65535;
 
-int totalMoney = 15531;
+int totalMoney;
 
 UINT16 withdraw(UINT16 n) {
 	if (totalMoney < n) return 0;
@@ -26,8 +26,26 @@ void withdrawAll() {
 	}
 }
 
+UINT16 withdrawAndReturnAll() {
+	UINT16 money, all;
+
+	money = MAX_UINT16;
+	all = 0;
+	while (money) {
+		if (withdraw(money) == 0) money /= 2;
+		else all += money;
+		cout << money << ": " << totalMoney << "\n";
+	}
+
+	return all;
+}
+
 int main(int argc, char* argv[]) {
+	totalMoney = 15531;
 	withdrawAll();
+
+	totalMoney = 15531;
+	cout << withdrawAndReturnAll() << "\n";
 
 	return 0;
 }
