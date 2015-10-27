9b830125cc576d24058de057e731bf62b5165ad5
kyduke <kyduke@gmail.com>
2015-10-28 00:38:48 +0900

    Create remainGreaterCount.cpp

---
+++ CareerCup/google/remainGreaterCount.cpp
@@ -0,0 +1,46 @@
+// http://careercup.com/question?id=5631660689195008
+
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+void printArray(vector<int>& arr) {
+	int i;
+
+	for (i = 0; i < arr.size(); i++) {
+		cout << arr[i];
+		if (i != arr.size() - 1) cout << ", ";
+	}
+	cout << "\n";
+}
+
+vector<int> remainGreaterCount(vector<int>& arr) {
+	multiset<int> s;
+	multiset<int>::iterator it;
+	vector<int> result;
+	int i, j;
+
+	result.assign(arr.size(), 0);
+	for (i = arr.size() - 1; i >= 0; i--) {
+		s.insert(arr[i]);
+		it = s.upper_bound(arr[i]);
+		result[i] = s.size() - distance(s.begin(), it);
+	}
+
+	return result;
+}
+
+int main(int argc, char* argv[]) {
+	int data[] = {3, 4, 5, 9, 2, 1, 3};
+	vector<int> arr(data, data + 7);
+	vector<int> result;
+
+	result = remainGreaterCount(arr);
+
+	printArray(arr);
+	printArray(result);
+
+	return 0;
+}
