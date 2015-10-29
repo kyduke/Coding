f42b08bb9c6c9ecb4d60f9d91f59f9cc0612e7c0
kyduke <kyduke@gmail.com>
2015-10-29 23:43:36 +0900

    Create printBigPair.cpp

---
+++ CareerCup/unknown/printBigPair.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printBigPair(vector<int>& arr, int N) {
+    unsigned int i, j;
+
+    for (i = 0; i < arr.size(); i++) {
+        for (j = i + 1; j < arr.size(); j++) {
+            if (arr[i] + arr[j] == N) cout << "(" << arr[i] << ", " << arr[j] << ") ";
+        }
+    }
+    cout << "\n";
+}
+
+int main(int argc, char* argv[]) {
+    int data[] = {3, 7, 2, 5, 6, 4};
+    vector<int> arr(data, data + 6);
+
+    printBigPair(arr, 9);
+
+    return 0;
+}
