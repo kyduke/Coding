4ced6a6c24c437d80cc43d853556cb5684872c4d
kyduke <kyduke@gmail.com>
2015-10-29 23:31:15 +0900

    Update findMissingNumber.cpp

--- CareerCup/unknown/findMissingNumber.cpp
+++ CareerCup/unknown/findMissingNumber.cpp
@@ -18,7 +18,7 @@ int main(int argc, char* argv[]) {
     int data[] = {0, 1, 2, 3, 4, 9, 5, 7, 6};
     vector<int> arr(data, data + 9);
 
-    cout << findMissingNumber(arr, 9) << "/n";
+    cout << findMissingNumber(arr, 9) << "\n";
 
     return 0;
 }
