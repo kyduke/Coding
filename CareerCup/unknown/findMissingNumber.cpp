671c702b3652b3b5d98432db52896c5c9f8a1e57
kyduke <kyduke@gmail.com>
2015-10-29 23:09:17 +0900

    Create findMissingNumber.cpp

---
+++ CareerCup/unknown/findMissingNumber.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int findMissingNumber(vector<int>& arr, int N) {
+    int i;
+
+    i = N;
+    while (i--) {
+        N ^= i ^ arr[i];
+    }
+
+    return N;
+}
+
+int main(int argc, char* argv[]) {
+    int data[] = {0, 1, 2, 3, 4, 9, 5, 7, 6};
+    vector<int> arr(data, data + 9);
+
+    cout << findMissingNumber(arr, 9) << "/n";
+
+    return 0;
+}
