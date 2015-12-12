96983ab7d7d0e0e048bd6980613b3f98408b5b02
kyduke <kyduke@gmail.com>
2015-12-12 22:00:06 +0900

    Update nonDecreasingSequence.cpp

--- CareerCup/unknown/nonDecreasingSequence.cpp
+++ CareerCup/unknown/nonDecreasingSequence.cpp
@@ -5,35 +5,22 @@
 
 using namespace std;
 
-vector<int> nonDecreasingSequence(vector<int>& a, vector<int>& b) {
-	vector<int> ret;
-	int i, n, prev, small, big;
-
-	n = a.size();
-	ret.assign(n, 0);
-
-	prev = -1;
-	for (i = 0; i < n; i++) {
-		if (a[i] > b[i]) {
-			small = b[i];
-			big = a[i];
-		} else {
-			small = a[i];
-			big = b[i];
-		}
-
-		if (small >= prev) {
-			ret[i] = small;
-		} else if (big >= prev) {
-			ret[i] = big;
-		} else {
-			ret.clear();
-			break;
-		}
-		prev = ret[i];
+bool nonDecreasingSequence(vector<int>& a, vector<int>& b, vector<int>&c, int idx, int bit) {
+	if (idx == a.size()) {
+		return bit == 3;
 	}
 
-	return ret;
+	if (idx == 0 || a[idx] >= c[idx - 1]) {
+		c[idx] = a[idx];
+		if (nonDecreasingSequence(a, b, c, idx + 1, bit | 1) == true) return true;
+	}
+
+	if (idx == 0 || b[idx] >= c[idx - 1]) {
+		c[idx] = b[idx];
+		if (nonDecreasingSequence(a, b, c, idx + 1, bit | 2) == true) return true;
+	}
+
+	return false;
 }
 
 int main(int argc, char* argv[]) {
@@ -45,18 +32,18 @@ int main(int argc, char* argv[]) {
 		cin >> n;
 		a.assign(n, 0);
 		b.assign(n, 0);
+		c.assign(n, 0);
 
 		for (i = 0; i < n; i++) cin >> a[i];
 		for (i = 0; i < n; i++) cin >> b[i];
 
-		c = nonDecreasingSequence(a, b);
-		if (c.size() == 0) {
-			cout << "IMPOSSIBLE\n";
-		} else {
+		if (nonDecreasingSequence(a, b, c, 0, 0) == true) {
 			for (i = 0; i < n; i++) {
 				cout << c[i] << " ";
 			}
 			cout << "\n";
+		} else {
+			cout << "IMPOSSIBLE\n";
 		}
 	}
 
