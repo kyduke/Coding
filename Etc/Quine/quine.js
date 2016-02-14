acd5f8e241b63dd5f913fb7b21c30e4b3cd29517
kyduke <kyduke@gmail.com>
2016-02-14 15:03:45 +0900

    Create quine.js

---
+++ Etc/Quine/quine.js
@@ -0,0 +1,1 @@
+a = "'";b = '"';c = 'console.log("a = " + b + a + b + ";b = " + a + b + a + ";c = " + a + c + a + ";" + c);';console.log("a = " + b + a + b + ";b = " + a + b + a + ";c = " + a + c + a + ";" + c);
