320d85c2f849478c4101efb45d20b1a9580ce923
kyduke <kyduke@gmail.com>
2016-04-22 14:52:01 +0900

    Update BFFs.cpp

--- GoogleCodeJam/Round1A2016/BFFs.cpp
+++ GoogleCodeJam/Round1A2016/BFFs.cpp
@@ -41,6 +41,55 @@ int solve(vector<int> BFFs) {
 	return max(circle, sum);
 }
 
+int solveN2(vector<int> BFFs) {
+	vector<int> DBFFs;
+	vector<int> visited;
+	vector<int> used;
+	int circle, i, j, current, next, end, count, sum;
+
+	DBFFs.assign(BFFs.size(), 0);
+	visited.assign(BFFs.size(), 0);
+
+	circle = 0;
+	for (i = 0; i < BFFs.size(); i++) {
+		if (visited[i]) continue;
+
+		used.assign(BFFs.size(), 0);
+		current = i;
+		next = BFFs[current];
+		end = i;
+		count = 0;
+		while (!used[next]) {
+			used[current] = 1;
+			end = current;
+			current = next;
+			next = BFFs[current];
+			count++;
+		}
+
+		if (next == end) {
+			DBFFs[end] = max(DBFFs[end], count);
+
+			for (j = 0; j < used.size(); j++) {
+				visited[j] |= used[j];
+			}
+		} else if (next == i) {
+			circle = max(circle, count + 1);
+
+			for (j = 0; j < used.size(); j++) {
+				visited[j] |= used[j];
+			}
+		}
+	}
+
+	sum = 0;
+	for (i = 0; i < DBFFs.size(); i++) {
+		sum += DBFFs[i];
+	}
+
+	return max(circle, sum);
+}
+
 int main(int argc, char* argv[]) {
 	int T, t, n, i;
 	vector<int> BFFs;
@@ -54,7 +103,7 @@ int main(int argc, char* argv[]) {
 			cin >> BFFs[i];
 			BFFs[i]--;
 		}
-		cout << "Case #" << ++t << ": " << solve(BFFs) << "\n";
+		cout << "Case #" << ++t << ": " << solve(BFFs) << " , " << solveN2(BFFs) << "\n";
 	}
 
 	return 0;
