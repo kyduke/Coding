86610f3e3d2d9949d98ecf410aee8fdad18976ec
kyduke <kyduke@gmail.com>
2016-05-02 10:35:00 +0900

    Update CloseMatch.cpp

--- GoogleCodeJam/Round1B2016/CloseMatch.cpp
+++ GoogleCodeJam/Round1B2016/CloseMatch.cpp
@@ -5,62 +5,149 @@
 
 using namespace std;
 
-void solve(string a, string b) {
-	string x, y;
-	int i, comp;
+string diff(string a, string b) {
+	string x, y, z;
+	int i, m;
 
-	x = "";
-	y = "";
-	comp = 0;
-	i = 0;
-	while (i < a.size()) {
-		if (comp == 0) {
-			if (a[i] == '?' && b[i] == '?') {
-				x += '0';
-				y += '0';
-			} else if (a[i] == '?' && b[i] != '?') {
-				x += b[i];
-				y += b[i];
-			} else if (a[i] != '?' && b[i] == '?') {
-				x += a[i];
-				y += a[i];
-			} else {
-				x += a[i];
-				y += b[i];
-				if (a[i] > b[i]) {
-					comp = 1;
-				} else if (a[i] < b[i]) {
-					comp = -1;
-				}
-			}
-		} else if (comp == 1) {
-			if (a[i] == '?') {
-				x += '0';
-			} else {
-				x += a[i];
-			}
-			if (b[i] == '?') {
-				y += '9';
-			} else {
-				y += b[i];
-			}
-		} else {
-			if (a[i] == '?') {
-				x += '9';
-			} else {
-				x += a[i];
-			}
-			if (b[i] == '?') {
-				y += '0';
-			} else {
-				y += b[i];
-			}
-		}
-
-		i++;
+	x = a;
+	y = b;
+	if (a < b) {
+		x = b;
+		y = a;
 	}
 
-	cout << x << " " << y;
+	m = 0;
+	z = a;
+	for (i = a.size() - 1; i >= 0; i--) {
+		m += x[i] - y[i];
+		if (m < 0) {
+			z[i] = (m + 10) + '0';
+			m = -1;
+		} else {
+			z[i] = m + '0';
+			m = 0;
+		}
+	}
+
+	return z;
+}
+
+pair<string, string> findSmall(pair<string, string> a, pair<string, string> b) {
+	pair<string, string> result;
+	string diffA, diffB;
+
+	diffA = diff(a.first, a.second);
+	diffB = diff(b.first, b.second);
+	result = a;
+	if (diffA > diffB) {
+		result = b;
+	} else if (diffA == diffB) {
+		if (a.first > b.first) {
+			result = b;
+		} else if (a.first == b.first && a.second > b.second) {
+			result = b;
+		}
+	}
+
+	return result;
+}
+
+pair<string, string> makeBigger(string a, string b, bool left) {
+	pair<string, string> result;
+	char x, y;
+	int i;
+
+	if (left) {
+		x = '0';
+		y = '9';
+	} else {
+		x = '9';
+		y = '0';
+	}
+
+	result.first = a;
+	result.second = b;
+	for (i = 0; i < a.size(); i++) {
+		if (a[i] == '?') result.first[i] = x;
+		if (b[i] == '?') result.second[i] = y;
+	}
+
+	return result;
+}
+
+pair<string, string> makeEqual(string a, string b, int idx) {
+	pair<string, string> result, temp;
+
+	if (idx == a.size()) {
+		result.first = a;
+		result.second = b;
+		return result;
+	}
+
+	if (a[idx] == '?' && b[idx] == '?') {
+		a[idx] = '0';
+		b[idx] = '0';
+		result = makeEqual(a, b, idx + 1);
+
+		a[idx] = '1';
+		b[idx] = '0';
+		result = findSmall(result, makeBigger(a, b, true));
+
+		a[idx] = '0';
+		b[idx] = '1';
+		result = findSmall(result, makeBigger(a, b, false));
+
+		a[idx] = '?';
+		b[idx] = '?';
+	} else if (a[idx] == '?' && b[idx] != '?') {
+		a[idx] = b[idx];
+		result = makeEqual(a, b, idx + 1);
+
+		if (b[idx] != '9') {
+			a[idx] = b[idx] + 1;
+			result = findSmall(result, makeBigger(a, b, true));
+		}
+
+		if (b[idx] != '0') {
+			a[idx] = b[idx] - 1;
+			result = findSmall(result, makeBigger(a, b, false));
+		}
+
+		a[idx] = '?';
+	} else if (a[idx] != '?' && b[idx] == '?') {
+		b[idx] = a[idx];
+		result = makeEqual(a, b, idx + 1);
+
+		if (a[idx] != '9') {
+			b[idx] = a[idx] + 1;
+			result = findSmall(result, makeBigger(a, b, false));
+		}
+
+		if (a[idx] != '0') {
+			b[idx] = a[idx] - 1;
+			result = findSmall(result, makeBigger(a, b, true));
+		}
+
+		b[idx] = '?';
+	} else if (a[idx] == b[idx]) {
+		result = makeEqual(a, b, idx + 1);
+	} else {
+		if (a[idx] > b[idx]) {
+			result = makeBigger(a, b, true);
+		} else {
+			result = makeBigger(a, b, false);
+		}
+	}
+
+	return result;
+}
+
+void solve(string a, string b) {
+	pair<string, string> result;
+
+	result = makeEqual(a, b, 0);
+
+	cout << result.first << " " << result.second;
 }
 
 int main(int argc, char* argv[]) {
@@ -91,4 +178,13 @@ Case #1: 19 20
 Case #2: 023 023
 Case #3: 0 0
 Case #4: 05 00
+
+3
+93 ?8
+?9 ?0
+??9 6?1
+=====
+98 88
+09 10
+599 601
 */
