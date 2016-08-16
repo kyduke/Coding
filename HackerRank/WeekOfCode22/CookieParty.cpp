5bf182345e5d117fa9c04d4c8124c3df416aec66
kyduke <kyduke@gmail.com>
2016-08-17 00:48:43 +0900

    Update CookieParty.cpp

--- HackerRank/WeekOfCode22/CookieParty.cpp
+++ HackerRank/WeekOfCode22/CookieParty.cpp
@@ -1,33 +1,19 @@
 // https://www.hackerrank.com/contests/w22/challenges/cookie-party
 
-#include <map>
-#include <set>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <string>
-#include <bitset>
-#include <cstdio>
-#include <limits>
-#include <vector>
-#include <climits>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
-#include <numeric>
-#include <sstream>
 #include <iostream>
-#include <algorithm>
-#include <unordered_map>
 
 using namespace std;
 
 int main(){
     int n;
     int m;
+    int r;
     cin >> n >> m;
+    
+    r = m % n;
+    if (r != 0) r = n - r;
+    
+    cout << r;
+    
     return 0;
 }
