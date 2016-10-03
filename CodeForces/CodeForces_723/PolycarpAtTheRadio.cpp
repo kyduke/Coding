44e060fcdc964f90cea9dc56dccd63cfc3aa793f
kyduke <kyduke@gmail.com>
2016-10-04 01:41:50 +0900

    Update PolycarpAtTheRadio.cpp

--- CodeForces/CodeForces_723/PolycarpAtTheRadio.cpp
+++ CodeForces/CodeForces_723/PolycarpAtTheRadio.cpp
@@ -1,6 +1,8 @@
 // http://codeforces.com/contest/723/problem/C
 
 #include <iostream>
+#include <vector>
+#include <map>
 
 using namespace std;
 
@@ -8,85 +10,58 @@ const int SIZE = 2001;
 
 int arr[SIZE] = {0, };
 int count[SIZE] = {0, };
-int diff[SIZE] = {0, };
-int answers[SIZE] = {0, };
+vector<int> idxs[SIZE];
+vector<int> others;
 
 void solve(int n, int m, int other) {
-	int quota, remain, inner, ans, i, k;
+	multimap<int, int> mm;
+	multimap<int, int>::iterator it;
+	multimap<int, int>::iterator it2;
+	int quota, ans, i, k, a, b, c, d;
 
 	quota = n / m;
-	remain = n % m;
-	inner = 0;
 	ans = 0;
 
 	for (i = 1; i <= m; i++) {
-		while (true) {
-			if (diff[i] == quota) break;
-			if (diff[i] < quota) {
-				if (inner > 0) {
-					inner--;
-				} else if (other > 0) {
-					other--;
-					ans++;
-				} else {
-					inner--;
-					ans++;
-				}
-				diff[i]++;
-			} else if (diff[i] == quota + 1) {
-				if (remain > 0) {
-					remain--;
-					break;
-				}
-				if (inner < 0) {
-					inner++;
-				} else {
-					inner++;
-					ans++;
-				}
-				diff[i]--;
-				arr[i] = 0;
-			} else {
-				if (inner < 0) {
-					inner++;
-				} else {
-					inner++;
-					ans++;
-				}
-				diff[i]--;
-				arr[i] = 0;
-			}
+		if (count[i] < quota) {
+			ans += quota - count[i];
 		}
+		mm.insert(make_pair(count[i], i));
 	}
 
-	for (i = 1; i <= m; i++) {
-		if (remain == 0) break;
-		if (diff[i] == quota) {
-			remain--;
-			diff[i]++;
-		}
-	}
-
-	k = 0;
-	for (i = 1; i <= m; i++) {
-		if (diff[i] > count[i]) {
-			diff[i] -= count[i];
-			while (diff[i]--) {
-				answers[k] = i;
-				k++;
-			}
-		} else {
-			diff[i] = 0;
-		}
-	}
-
-	k = 0;
 	for (i = 0; i < n; i++) {
-		if (k == ans) break;
-		if (arr[i] == 0) {
-			arr[i] = answers[k];
-			k++;
+		if (arr[i] <= m) continue;
+		it = mm.begin();
+		if (it->first < quota) {
+			a = it->first;
+			b = it->second;
+			arr[i] = b;
+			mm.erase(it);
+			mm.insert(make_pair(a + 1, b));
 		}
+	}
+
+	while (true) {
+		it = mm.begin();
+		if (it->first >= quota) break;
+		a = it->first;
+		b = it->second;
+		mm.erase(it);
+
+		it2 = mm.begin();
+		k = mm.size() - 1;
+		while (k--) {
+			it2++;
+		}
+		c = it2->first;
+		d = it2->second;
+		mm.erase(it2);
+
+		arr[ idxs[d][0] ] = b;
+		idxs[d].erase(idxs[d].begin());
+
+		mm.insert(make_pair(a + 1, b));
+		mm.insert(make_pair(c - 1, d));
 	}
 
 	cout << quota << " " << ans << "\n";
@@ -103,12 +78,13 @@ int main(int argc, char* argv[]) {
 	cin >> n >> m;
 	for (i = 0; i < n; i++) {
 		cin >> a;
+		arr[i] = a;
 		if (a <= m) {
-			arr[i] = a;
 			count[a]++;
-			diff[a]++;
+			idxs[a].push_back(i);
 		} else {
 			other++;
+			others.push_back(i);
 		}
 	}
 
@@ -135,4 +111,28 @@ int main(int argc, char* argv[]) {
 ======
 1 4
 1 2 3 4 
+
+5 4
+3 1 495987801 522279660 762868488
+=====
+1 2
+3 1 4 2 762868488
+
+20 3
+3 2 2 3 3 3 2 3 3 3 2 748578511 149249674 844954396 321901094 3 255089924 244803836 3 943090472
+=====
+6 8
+3 2 2 1 3 3 2 3 3 3 2 1 1 1 1 3 1 2 3 2
+
+50 10
+820558149 7 10 7 9 9 7 7 7 9 7 10 8 8 10 8 6 8 9 9 8 971788012 9 8 9 10 6 5 871178015 4 10 5 7 9 10 9 10 4 643998638 8 10 9 10 766953454 5 9 10 10 8 10
+=====
+5 23
+1 1 1 1 1 2 7 7 7 2 7 2 2 2 3 3 6 8 3 3 8 3 4 8 9 4 6 5 4 4 5 5 7 9 5 9 6 4 6 8 10 9 10 6 5 9 10 10 8 10
+
+7 4
+1 1 1 1 1 1 1
+=====
+1 3
+2 3 4 1 1 1 1
 */
