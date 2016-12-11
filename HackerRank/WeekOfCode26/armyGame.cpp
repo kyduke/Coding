a48b1f9223aed5a1377798bca4106f98f0684466
kyduke <kyduke@gmail.com>
2016-12-11 20:06:37 +0900

    Create armyGame.cpp

---
+++ HackerRank/WeekOfCode26/armyGame.cpp
@@ -0,0 +1,22 @@
+// https://www.hackerrank.com/contests/w26/challenges/game-with-cells
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    int n, m;
+    
+    cin >> n >> m;
+    
+    cout << ((n + 1) / 2) * ((m + 1) / 2) << "\n";
+    
+    return 0;
+}
+
+/*
+2 2
+=====
+1
+*/
