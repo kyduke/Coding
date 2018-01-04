cfa8e0fa15643e3fccd09e19d4ecf107fed8fd54
kyduke <kyduke@gmail.com>
2018-01-04 22:32:42 +0900

    Adds a faster solution

--- LeetCode/014_LongestCommonPrefix.cpp
+++ LeetCode/014_LongestCommonPrefix.cpp
@@ -29,3 +29,24 @@ public:
         return ans;
     }
 };
+
+// 2018-01-04 fastre solution
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans;
+        int i, j;
+        
+        if (strs.size() == 0) return ans;
+        
+        ans = strs[0];
+        for (i = 1; i < strs.size(); i++) {
+            for (j = 0; j < min(ans.size(), strs[i].size()); j++) {
+                if (ans[j] != strs[i][j]) break;
+            }
+            ans = ans.substr(0, min((int)ans.size(), j));
+        }
+        
+        return ans;
+    }
+};
