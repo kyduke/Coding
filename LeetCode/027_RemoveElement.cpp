cd389f333a993dcc69c8cd3c2bfcceb1070e5347
kyduke <kyduke@gmail.com>
2018-01-24 00:10:26 +0900

    Update 027_RemoveElement.cpp

--- LeetCode/027_RemoveElement.cpp
+++ LeetCode/027_RemoveElement.cpp
@@ -19,3 +19,29 @@ public:
         return ans;
     }
 };
+
+// 2018-01-24
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int ans, i, j;
+        
+        ans = 0;
+        j = -1;
+        for (i = 0; i < nums.size(); i++) {
+            if (nums[i] == val) {
+                if (j == -1) {
+                    j = i;
+                }
+            } else {
+                ans++;
+                if (j != -1) {
+                    nums[j] = nums[i];
+                    j++;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
