e1ed6e925d63c878ea84598b6272cb5847fdec05
kyduke <kyduke@gmail.com>
2018-01-30 15:15:55 +0900

    Update checkParentheses.cpp

--- Etc/checkParentheses.cpp
+++ Etc/checkParentheses.cpp
@@ -6,51 +6,84 @@
 using namespace std;
 
 bool checkParentheses(string& str) {
-	int i, a, b;
+    int i, a, b;
+    
+    a = 0;
+    b = 0;
+    for (i = 0; i < str.size(); i++) {
+        if (str[i] == ')') {
+            a++;
+        } else {
+            b++;
+        }
+        if (a > b) {
+            return false;
+        }
+    }
+    
+    a = 0;
+    b = 0;
+    for (i = str.size() - 1; i >= 0; i--) {
+        if (str[i] == '(') {
+            a++;
+        } else {
+            b++;
+        }
+        if (a > b) {
+            return false;
+        }
+    }
+    
+    return true;
+}
 
-	a = 0;
-	b = 0;
-	for (i = 0; i < str.size(); i++) {
-		if (str[i] == ')') {
-			a++;
-		} else {
-			b++;
-		}
-		if (a > b) {
-			return false;
-		}
-	}
-
-	a = 0;
-	b = 0;
-	for (i = str.size() - 1; i >= 0; i--) {
-		if (str[i] == '(') {
-			a++;
-		} else {
-			b++;
-		}
-		if (a > b) {
-			return false;
-		}
-	}
-
-	return true;
+bool checkParenthesesFast(string& str) {
+    int i, question, count;
+    
+    question = 0;
+    count = 0;
+    for (i = 0; i < str.size(); i++) {
+        if (str[i] == '(') {
+            count++;
+        } else if (str[i] == ')') {
+            count--;
+        } else {
+            question++;
+            count--;
+        }
+        
+        while (question > 0 && count < 0) {
+            question--;
+            count += 2;
+        }
+        
+        if (count < 0) {
+            return false;
+        }
+    }
+    
+    return count == 0;
 }
 
 int main(int argc, char* argv[]) {
-	string str;
-
-	str = "((()))";
-	cout << str << ": " << checkParentheses(str) << "\n";
-
-	str = "()()()";
-	cout << str << ": " << checkParentheses(str) << "\n";
-
-	str = "(?????";
-	cout << str << ": " << checkParentheses(str) << "\n";
-
-	str = "(?)))?";
-	cout << str << ": " << checkParentheses(str) << "\n";
-
-	return 0;
+    string str;
+    
+    str = "((()))";
+    cout << str << ": " << checkParentheses(str) << "\n";
+    cout << str << ": " << checkParenthesesFast(str) << "\n";
+    
+    str = "()()()";
+    cout << str << ": " << checkParentheses(str) << "\n";
+    cout << str << ": " << checkParenthesesFast(str) << "\n";
+    
+    str = "(?????";
+    cout << str << ": " << checkParentheses(str) << "\n";
+    cout << str << ": " << checkParenthesesFast(str) << "\n";
+    
+    str = "(?)))?";
+    cout << str << ": " << checkParentheses(str) << "\n";
+    cout << str << ": " << checkParenthesesFast(str) << "\n";
+    
+    return 0;
 }
+
